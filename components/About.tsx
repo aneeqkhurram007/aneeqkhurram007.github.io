cbfced42370d834f287cc8d03b1a543aaf5e2127
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2022-10-31 12:59:03 +0500

    about and skill images added

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { PageInfo } from "../typings";
 import { urlFor } from "../sanity";
+import Image from "next/image";
 
 type Props = {
   pageInfo: PageInfo;
@@ -13,9 +14,9 @@ function About({ pageInfo }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="min-h-screen flex relative space-y-4 sm:space-y-10
+      className="h-[70vh] lg:h-screen flex relative space-y-4 sm:space-y-10
   flex-col text-center md:text-left xl:flex-row max-w-7xl
-  mx-auto px-10 justify-center sm:justify-evenly items-center"
+  mx-auto px-10 justify-center sm:justify-evenly  items-center"
     >
       <h3
         className="uppercase tracking-[20px]
@@ -23,18 +24,24 @@ function About({ pageInfo }: Props) {
       >
         About
       </h3>
-
-      <motion.img
+      <motion.div
         initial={{ x: -200, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
         transition={{ duration: 1.2 }}
-        src={urlFor(pageInfo?.profilePic).url()}
-        alt="Profile"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 sm:w-56 sm:h-56 rounded-full object-cover
-        md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
-      />
-      <div className="space-y-10 px-0 md:px-10">
+        className="-mb-20 md:mb-0 flex-shrink-0 w-40 h-40 sm:w-56 sm:h-56 rounded-full
+lg:rounded-lg md:w-64 xl:w-[500px] xl:h-[600px] "
+      >
+        <Image
+          src={urlFor(pageInfo?.profilePic).url()}
+          alt="Profile"
+          objectFit="cover"
+          width={1000}
+          height={1000}
+          className="rounded-full lg:rounded-lg"
+        />
+      </motion.div>
+      <div className=" px-0 md:px-10">
         <h4 className="text-4xl font-semibold">
           Here is a{" "}
           <span className="underline decoration-[#f7ab0a]">little</span>{" "}
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Skill } from "../typings";
 import { urlFor } from "../sanity";
+import Image from "next/image";
 
 type Props = {
   direction?: boolean;
@@ -11,17 +12,25 @@ type Props = {
 function Skill({ direction, skill }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
+      <motion.div
         initial={{
           x: direction ? -200 : 200,
           opacity: 0,
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src={urlFor(skill.image).url()}
-        className="rounded-full border border-gray-500 object-cover w-10 h-10 xl:w-32
+        className="rounded-full border border-gray-500 flex object-cover w-10 h-10 xl:w-32
     xl:h-32 filter group-hover:grayscale md:h-28 md:w-28 transition duration-300 ease-in-out"
-      />
+      >
+        <Image
+          src={urlFor(skill.image).url()}
+          alt={skill.title}
+          objectFit="fill"
+          className="rounded-full"
+          width={500}
+          height={500}
+        />
+      </motion.div>
       <div
         className="absolute opacity-0 group-hover:opacity-80 transition duration-300
       ease-in-out group-hover:bg-white h-10 w-10 md:h-28 md:w-28 xl:w-32 xl:h-32
